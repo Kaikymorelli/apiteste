f81a8b886d777bca5fdb59919735e446081a4bbe
Kaikymorelli <132359674+Kaikymorelli@users.noreply.github.com>
2023-09-14 16:38:35 -0300

    Add files via upload

---
+++ projetospringproduto/src/main/java/com/projetoproduto/ProjetospringprodutoApplication.java
@@ -0,0 +1,13 @@
+package com.projetoproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetospringprodutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetospringprodutoApplication.class, args);
+	}
+
+}
---
+++ projetospringproduto/src/test/java/com/projetoproduto/ProjetospringprodutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetospringprodutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
